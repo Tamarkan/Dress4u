af9804277c8fb6e8ce0dddeea246d31e6747b541
tamar sh <t6787586@gmail.com>
2023-01-15 18:13:21 +0200

    try

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors=require('cors');
 const port =8000;
-app.use(cors())
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
